87d74d83f29cded4db2bbf4a869e99dbfeae2bf8
IntrntSrfr <aleksander@tosbakken.com>
2023-03-06 23:22:24 +0100

    bugfix add export to gif

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use rand::prelude::*;
 use std::{thread, time};
+use std::borrow::Cow;
+use std::fs::File;
+use gif::{Encoder, Frame, Repeat};
 
 fn main() {
-    let mut g = Game::new(16, 32, true).unwrap();
-    g.run(4);
-    //println!("{}", (-1i32).rem_euclid(10));
+    let mut g = Game::new(16, 16, true).unwrap();
+    g.run(250, true, 250);
+    g.export(16, 16)
 }
 
 #[derive(Debug)]
@@ -25,27 +28,45 @@ impl Game {
         })
     }
 
-    fn run(&mut self, iterations: i32) {
+    fn run(&mut self, iterations: i32, display: bool, delay: u64) {
         for _ in 0..iterations {
-            self.grid.display();
-            self.step();
+            if display { self.grid.display() }
             self.history.push(self.buf_grid.clone());
-            self.grid = self.buf_grid.clone();
-            thread::sleep(time::Duration::from_millis(500));
+            self.step();
+            if delay != 0{
+                thread::sleep(time::Duration::from_millis(delay));
+            }
         }
     }
 
     fn step(&mut self) {
         //self.grid = self.buf_grid.clone()
-        self.grid.step(&mut self.buf_grid)
+        self.grid.step(&mut self.buf_grid);
+        self.grid = self.buf_grid.clone();
+
     }
 
-    fn export(&mut self) {}
+    fn export(&self, h: u16, w: u16) {
+        let palette =&[0xFF, 0xFF, 0xFF, 0, 0, 0];
+        let mut f = File::create("./out.gif").unwrap();
+        let mut encoder = Encoder::new(&mut f, w, h, palette).unwrap();
+        encoder.set_repeat(Repeat::Infinite).unwrap();
+
+        for g in &self.history{
+            let mut frame = Frame::default();
+            frame.delay = 25;
+            frame.width = w;
+            frame.height = h;
+            frame.buffer = Cow::from_iter(g.clone().data.into_iter());
+            //println!("{:?}", frame);
+            encoder.write_frame(&frame).unwrap()
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
 struct Grid {
-    data: Vec<bool>,
+    data: Vec<u8>,
     height: i32,
     width: i32,
     wrap: bool
@@ -57,7 +78,7 @@ impl Grid {
             return Err("grid must be at least 16x16".to_string());
         }
         return Ok(Grid {
-            data: std::iter::repeat(false).take((h * w) as usize).collect(),
+            data: std::iter::repeat(0).take((h * w) as usize).collect(),
             height: h,
             width: w,
             wrap
@@ -67,7 +88,7 @@ impl Grid {
     fn randomize(&mut self) -> &mut Self {
         for y in 0..self.height {
             for x in 0..self.width {
-                self.set(x, y, random::<f32>() < 0.5)
+                self.set(x, y, if random::<f32>() < 0.5 { 0 } else { 1 })
             }
         }
         return self;
@@ -78,11 +99,11 @@ impl Grid {
             for x in 0..self.width {
                 let n = self.neighbours(x, y);
                 let at = self.at(x, y);
-                if at && (n != 2 && n != 3) {
-                    dst.set(x, y, false)
+                if at == 1 && (n != 2 && n != 3) {
+                    dst.set(x, y, 0)
                 }
-                if !at && n == 3 {
-                    dst.set(x, y, true)
+                if at == 0 && n == 3 {
+                    dst.set(x, y, 1)
                 }
             }
         }
@@ -92,17 +113,17 @@ impl Grid {
         return (x.rem_euclid(self.width), y.rem_euclid(self.height));
     }
 
-    fn at(&self, mut x: i32, mut y: i32) -> bool {
-        if !self.wrap && (x < 0 || x >= self.width as i32 || y < 0 || y >= self.height as i32) {
-            return false;
+    fn at(&self, mut x: i32, mut y: i32) -> u8 {
+        if !self.wrap && (x < 0 || x >= self.width || y < 0 || y >= self.height ) {
+            return 0;
         }
         (x, y) = self.get_wrapped_pos(x, y);
 
         self.data[(y * self.width + x) as usize]
     }
 
-    fn set(&mut self, mut x: i32, mut y: i32, value: bool) {
-        if !self.wrap && (x < 0 || x >= self.width as i32 || y < 0 || y >= self.height as i32) {
+    fn set(&mut self, mut x: i32, mut y: i32, value: u8) {
+        if !self.wrap && (x < 0 || x >= self.width || y < 0 || y >= self.height) {
             return;
         }
         (x, y) = self.get_wrapped_pos(x, y);
@@ -113,8 +134,8 @@ impl Grid {
         let mut count = 0;
         for dy in -1..2 {
             for dx in -1..2 {
-                if dy == dx {
-                    continue;
+                if dy == 0 && dx == 0 {
+                    continue
                 }
                 count += self.at(x + dx, y + dy) as i32
             }
@@ -126,7 +147,7 @@ impl Grid {
         let mut out = String::new();
         for y in 0..self.height {
             for x in 0..self.width {
-                if self.at(x, y) {
+                if self.at(x, y) == 1 {
                     out.push_str("\u{001b}[34m# ")
                 } else {
                     out.push_str("\u{001b}[31m. ")
